aa500dbdf53cca18d984943f87c49a091b52831d
Nguyen Hong Hai <hainhph35304@fpt.edu.vn>
2024-04-23 16:07:25 +0700

    Xây base

--- DuAnBanHang.API/Program.cs
+++ DuAnBanHang.API/Program.cs
@@ -1,35 +1,36 @@
-namespace DuAnBanHang.API
+
+using BaseSolution.Infrastructure.Extensions;
+using DuAnBanHang.Domain.Mapper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplication();
+builder.Services.AddLocalization(builder.Configuration);
+
+builder.Services.AddEventBus(builder.Configuration);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
